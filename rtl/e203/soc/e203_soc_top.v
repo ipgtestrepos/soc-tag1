ed1f701a922166ba24d6c1ca2e031f68e84acfbf
Dan FitzPatrick <dan.fitzpatrick@silvaco.com>
2024-06-16 20:20:19 -0400

    version 1.0.1 commit

--- rtl/e203/soc/e203_soc_top.v
+++ rtl/e203/soc/e203_soc_top.v
@@ -717,4 +717,35 @@ module e203_soc_top(
 
   );
 
+  ALU x(Z, Cout, D, A, B, Cin, S);
+
 endmodule
+
+
+module decoder24_gatelevel_tb;
+
+	wire [3:0] D;
+	reg x, y, E;
+	
+	decoder24_gatelevel dec24_gate(D, x, y, E);
+	
+	initial
+		begin
+			$display("2-4 Decoder : ");
+			$display("E\ty\tx\t|\tD");
+			$display("---------------------------------");
+			$monitor("%b\t%b\t%b\t|\t%b", E, y, x, D);
+			
+				E = 1'b0; y = 1'b0; x = 1'b0;
+			#10	E = 1'b0; y = 1'b0; x = 1'b1;
+			#10	E = 1'b0; y = 1'b1; x = 1'b0;
+			#10	E = 1'b0; y = 1'b1; x = 1'b1;
+			#10	E = 1'b1; y = 1'b0; x = 1'b0;
+			#10	E = 1'b1; y = 1'b0; x = 1'b1;
+			#10	E = 1'b1; y = 1'b1; x = 1'b0;
+			#10	E = 1'b1; y = 1'b1; x = 1'b1;
+		end
+
+endmodule
+
+
---
+++ rtl/vendor/dc/alu_8bit/FullAdder.v
@@ -0,0 +1,14 @@
+module FullAdder(s, cout, a, b, cin);
+	
+	output s, cout;
+	input a, b, cin;
+
+	wire x, y, z;
+	
+	xor x1(x, a, b);
+	and a1(y, a, b);
+	xor x2(s, x, cin);
+	and a2(z, x, cin);
+	or o1(cout, z, y);
+
+endmodule;
